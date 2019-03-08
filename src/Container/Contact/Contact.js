de2166a72801c0bc93015e14b6a6cfdf69f32036
github username <cjab86@gmail.com>
2019-03-08 11:38:57 -0500

    added contact form

--- src/Container/Contact/Contact.js
+++ src/Container/Contact/Contact.js
@@ -1,19 +1,116 @@
 import React, { Component } from 'react'
-import { Icon, Transition } from 'semantic-ui-react'
+import PropTypes from 'prop-types'
+import styles from './Contact.module.css'
+import { Icon, Transition, Button, Form } from 'semantic-ui-react'
 import { Route } from 'react-router-dom'
+import Success from './Success'
+import Failure from './Failure'
 // import { Parallax, Background } from 'react-parallax'
 import '../../App.css'
 
 // const image1 = "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
 
 export default class Contact extends Component {
-  state = { visible: false }
+  constructor(props){
+    super()
+
+    this.state = {
+      visible: false,
+      senderName: '',
+      email: '',
+      emailError: false,
+      message: '',
+      formSubmitted: false,
+      formError: false
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleCancel = this.handleCancel.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+  }
+
+  static sender = 'sender@example.com'
 
   toggleVisibility = () => this.setState({ visible: !this.state.visible })
 
   componentDidMount() {
     this.toggleVisibility()
   }
+
+  handleCancel() {
+    this.setState({
+      senderName: '',
+      email: '',
+      message: ''
+    })
+  }
+
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  validateEmail(email){
+      let re = /\S+@\S+\.\S+/;
+      return re.test(email);
+  }
+
+  handleSubmit(event){
+      event.preventDefault()
+
+      let error = false
+
+      if (this.state.email === '' || this.validateEmail(this.state.email) === false){
+        this.setState({ emailError: true })
+        error = true
+      } else {
+        this.setState({ emailError: false })
+      }
+
+      if (error){
+        this.setState({ formError: true })
+        return
+      }
+
+      this.setState({ formError: false })
+
+      const {
+        REACT_APP_EMAILJS_RECEIVER: receiverEmail,
+        REACT_APP_EMAILJS_TEMPLATEID: template
+      } = this.props.env
+
+      this.sendMessage(
+        template,
+        this.state.senderName,
+        this.state.email,
+        receiverEmail,
+        this.state.message)
+
+    }
+
+    sendMessage(templateId, senderName, senderEmail, receiverEmail, message){
+      window.emailjs.send(
+        'gmail',
+        templateId,
+        {
+          senderName,
+          senderEmail,
+          receiverEmail,
+          message
+        })
+        .then(res => {
+          this.setState({
+            senderName: '',
+            email: '',
+            message: '',
+            formSubmitted: true
+          })
+        })
+        // Handle errors here however you like, or use a React error boundary
+        .catch(err => console.error('Failed to send feedback. Error: ', err))
+    }
 
   render() {
 
@@ -22,12 +119,19 @@ export default class Contact extends Component {
     return (
       <Route path="/contact" render={() =>
         <Transition visible={visible} animation='fade' duration={800}>
-          <div className='block'>
+          <div className={styles['contact-form']}>
             <h1>Contact me</h1>
-
-            <p>
-              chris [d o t] buggelli [a t] gmail [d o t] com
-            </p>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Input required label='Name:' type='text' name='senderName' value={this.state.senderName} onChange={this.handleChange}>
+              </Form.Input>
+              <Form.Input required label='Email:' type='text' name='email' erro={this.state.emailError} value={this.state.email} onChange={this.handleChange}>
+              </Form.Input>
+              <Form.TextArea required label='Message:' type='text' name='message' value={this.state.message} onChange={this.handleChange}>
+              </Form.TextArea>
+              <Form.Button>Submit</Form.Button>
+            </Form>
+            <Success visible={this.state.formSubmitted} />
+            <Failure visible={this.state.formError} />
 
             <p>
               <Icon onClick={()=> window.open("https://www.linkedin.com/in/chrisbuggelli/", "_blank")} link name="linkedin" size="big" />
@@ -40,3 +144,7 @@ export default class Contact extends Component {
     )
   }
 }
+
+Contact.propTypes = {
+  env: PropTypes.object.isRequired
+};
